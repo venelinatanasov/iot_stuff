0f52f0956dfa9b2e060932949bc8bb8c23971c8d
StefanAntonov <stefanaantonov@abv.bg>
2021-03-12 19:42:55 +0200

    Code for station with DigiMesh

---
+++ SensorStation_DM.ino
@@ -0,0 +1,83 @@
+#include <Adafruit_AHTX0.h>
+#include <MAX44009.h>
+#include <Wire.h>
+
+Adafruit_AHTX0 aht;
+MAX44009 Lux(0x4A);
+
+void setup() {
+  Lux.Begin(0, 188000);
+  Wire.begin();
+  delay(1000);
+  
+  Serial.begin(9600);
+  if (!aht.begin()) {
+    Serial.println("Could not find AHT? Check wiring");
+    while (1){delay(10);}
+  }
+  Serial.println("--AHT10 or AHT20 found--");
+}
+
+void loop() {
+  sensors_event_t humidity, temp;
+  aht.getEvent(&humidity, &temp);// populate temp and humidity objects with fresh data
+  
+  Serial.print("SN-2:");//print Station Number
+  Serial.print("\nTemperature:   "); Serial.print(temp.temperature); Serial.println("C");//print Temperature
+  Serial.print("Humidity:      "); Serial.print(humidity.relative_humidity); Serial.println("%RH");//print Relative Humidity
+  Serial.print("Ambient light: "); Serial.print(Lux.GetLux()); Serial.println("lux");//print Ambient light
+  Serial.print("Gas: "); Serial.print((analogRead(A3)*100)/1024);Serial.println("%");
+  
+  delay(5000);
+}
+
+//Troubleshooting code with print two both Serial and Serial connections:
+/*
+#include <Adafruit_AHTX0.h>
+#include <MAX44009.h>
+#include <Wire.h>
+
+#define LEDpin 4
+
+Adafruit_AHTX0 aht;
+MAX44009 Lux(0x4A);
+
+void setup() {
+  Lux.Begin(0, 188000);
+  Wire.begin();
+  
+  pinMode(LEDpin, OUTPUT);
+  digitalWrite(LEDpin, HIGH);
+
+  delay(1000);
+  digitalWrite(LEDpin, LOW);
+  
+  Serial.begin(9600);
+  Serial.begin(9600);
+  if (!aht.begin()) {
+    Serial.println("Could not find AHT? Check wiring");
+    Serial.println("Could not find AHT? Check wiring");
+    while (1){delay(10);}
+  }
+  Serial.println("--AHT10 or AHT20 found--");
+  Serial.println("--AHT10 or AHT20 found--");
+}
+
+void loop() {
+  digitalWrite(LEDpin, LOW);
+  digitalWrite(LEDpin, HIGH);
+  sensors_event_t humidity, temp;
+  aht.getEvent(&humidity, &temp);// populate temp and humidity objects with fresh data
+  
+  Serial.print("SN-2:");//print Station Number
+  Serial.print("\nTemperature:   "); Serial.print(temp.temperature); Serial.println("C");//print Temperature
+  Serial.print("Humidity:      "); Serial.print(humidity.relative_humidity); Serial.println("%RH");//print Relative Humidity
+  Serial.print("Ambient light: "); Serial.print(Lux.GetLux()); Serial.println("lux");//print Ambient light
+
+  Serial.print("SN-2:");//print Station Number
+  Serial.print("\nTemperature:   "); Serial.print(temp.temperature); Serial.println("C");//print Temperature
+  Serial.print("Humidity:      "); Serial.print(humidity.relative_humidity); Serial.println("%RH");//print Relative Humidity
+  Serial.print("Ambient light: "); Serial.print(Lux.GetLux()); Serial.println("lux");//print Ambient light
+
+  delay(5000);
+}*/
