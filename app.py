6723fa7d9a553baba39a23ac8be4d0b62a4f0fd8
Iliana Genova <iliana.genova.02@gmail.com>
2021-03-13 16:28:05 +0200

    adding 'room' values model + local variables

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask, request, render_template, jsonify
 from flask_scss import Scss
 from flask_mqtt import Mqtt
 from flask_mongoengine import MongoEngine
+import time
 # import pymongo
 
 # import RPi.GPIO as GPIO
 # import serial
 # from os import system
-# import time
 # import multiprocessing
+starttime = time.time()
 
 app = Flask(__name__)
 app.testing = True
@@ -39,16 +40,62 @@ app.config['MQTT_PASSWORD'] = ''
 app.config['MQTT_REFRESH_TIME'] = 1.0  # refresh time in seconds
 mqtt = Mqtt(app)
 
-class Data(db.Document):
-		temperature = db.StringField()
-		light = db.StringField()
-		gas = db.StringField()
-		dust = db.StringField()
-		humidity = db.StringField() 
+
+
+#model for room_data to be saved in DB
+class DBData(db.Document):
+		temperature = db.IntField()
+		light = db.IntField()
+		gas = db.IntField()
+		dust = db.FloatField()
+		humidity = db.IntField() 
 
 		def to_json(self):
 			return{"temperature": self.temperature, "light": self.light, "gas": self.gas, "dust": self.dust, "humidity": self.humidity}
 
+#class to hold data locally
+class Room:
+	def __init__(self, temperature, light, gas, dust, humidity): 
+		self.temperature = temperature
+		self.light = light
+		self.gas = gas
+		self.dust = dust
+		self.humidity = humidity 
+
+	def update(self, temperature, light, gas, dust, humidity): 
+		self.temperature = temperature
+		self.light = light
+		self.gas = gas
+		self.dust = dust
+		self.humidity = humidity 
+	
+	def updateTemperature(self, temperature): 
+		self.temperature = temperature
+
+	def updateLight(self, light): 
+		self.light = light
+	
+	def updateDust(self, dust): 
+		self.dust = dust
+	
+	def updateGas(self, gas): 
+		self.gas = gas
+
+	def updateHumidity(self, humidity): 
+		self.humidity = humidity
+		
+	def convertToPercent(self, var):
+		return var*100/4096
+
+	def convertDust(self, var):
+		return var*100/0.5
+
+room_info = Room(0, 0, 0, 0, 0)
+# print(room_info.gas)
+
+      
+
+#MQTT connection
 @mqtt.on_connect()
 def handle_connect(client, userdata, flags, rc):
 		mqtt.subscribe('room/#')
@@ -58,7 +105,8 @@ def handle_connect(client, userdata, flags, rc):
 		# mqtt.subscribe('room/dust')	 #0/0.5 mg/m^3
 		# mqtt.subscribe('room/temperature') #celsius
 		# mqtt.subscribe('room/humidity') #40%
-
+		
+#MQTT events
 @mqtt.on_message()
 def handle_mqtt_message(client, userdata, message):
 		data = dict(
@@ -66,17 +114,18 @@ def handle_mqtt_message(client, userdata, message):
 				payload=message.payload.decode()
 		)
 		
-		if(data['topic'] == "room/dust"):
-			print(data['payload'])
-		if(data['topic'] == "room/gas"):
-			print(data['payload'])
-		if(data['topic'] == "room/light"):
-			print(data['payload'])
-		if(data['topic'] == "room/temperature"):
-			print(data['payload'])
-		if(data['topic'] == "room/humidity"):
-			print(data['payload'])
+		# if(data['topic'] == "room/dust"):
+		# 	print(data['payload'])
+		# if(data['topic'] == "room/gas"):
+		# 	print(data['payload'])
+		# if(data['topic'] == "room/light"):
+		# 	print(data['payload'])
+		# if(data['topic'] == "room/temperature"):
+		# 	print(data['payload'])
+		# if(data['topic'] == "room/humidity"):
+		# 	print(data['payload'])
 
+#main route
 @app.route('/')
 def index():
 	return render_template('index.html')
