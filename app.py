20bb9e1d3b67bb383ebb405972c1986e179aece6
Iliana Genova <iliana.genova.02@gmail.com>
2021-03-14 03:39:32 +0200

    graph for temperature added

--- app.py
+++ app.py
@@ -3,16 +3,20 @@ from flask_scss import Scss
 from flask_mqtt import Mqtt
 from flask_mongoengine import MongoEngine
 from flask_socketio import SocketIO
-import time, threading
+import time, threading, requests
 import datetime as dt
+
 # import pymongo
+# from flask_pymongo import PyMongo
+# from model.pymongo_model import SimpleModel, DiffHistoryModelV1, DiffHistoryModelV2
+# from pymongo_document import documents
+# from pymodm import MongoModel, fields
 
 # import RPi.GPIO as GPIO
 # import serial
 # from os import system
 # import multiprocessing
 starttime = time.time()
-threads_arr = []
 
 app = Flask(__name__)
 app.testing = True
@@ -27,10 +31,11 @@ app.config['MONGODB_SETTINGS'] = {
 
 #setting up Mongo client 
 # DB_URI = "mongodb+srv://iliana:moje@autohome.acegw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-# app.config["MONGODB_HOST"] = DB_URI
+# app.config["MONGO_URI"] = DB_URI
 
 #setting up DB
 db = MongoEngine()
+# db = PyMongo(app)
 db.init_app(app)
 
 #setting up sass
@@ -46,7 +51,7 @@ mqtt = Mqtt(app)
 
 #setting up socketio
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, async_mode="threading")
+# socketio = SocketIO(app, async_mode="threading")
 
 #model for room_data to be saved in DB
 class DB_data(db.Document):
@@ -56,6 +61,9 @@ class DB_data(db.Document):
 		dust = db.FloatField()
 		humidity = db.FloatField() 
 		timestamp = db.DateTimeField()
+
+		def getTemp(self):
+			return self.temperature
 
 		def to_json(self):
 			return{"temperature": self.temperature, "light": self.light, "gas": self.gas, "dust": self.dust, "humidity": self.humidity, "timestamp": self.timestamp}
@@ -109,6 +117,7 @@ room_info = Room(0, 0, 0, 0, 0)
 @mqtt.on_connect()
 def handle_connect(client, userdata, flags, rc):
 		mqtt.subscribe('room/#')
+		mqtt.subscribe('lights/#')
 		print("connected")
 		# mqtt.subscribe('room/gas')	 #230/4096
 		# mqtt.subscribe('room/light') #546/4096
@@ -123,6 +132,8 @@ def handle_mqtt_message(client, userdata, message):
 				topic=message.topic,
 				payload=message.payload.decode()
 		)
+
+		print(data['topic'] + data['payload'])
 		
 		if(data['topic'] == "room/dust"):
 			room_info.update_dust(room_info.convert_dust(data['payload']))
@@ -134,6 +145,20 @@ def handle_mqtt_message(client, userdata, message):
 			room_info.update_temperature(data['payload'])
 		if(data['topic'] == "room/humidity"):
 			room_info.update_humidity(data['payload'])
+
+		
+		if(data['topic'] == "lights/google_on_off"):
+			print("in")
+			if(data['payload'] == '1'):
+				data = {'lights' : 1}
+				return jsonify({'lights' : 1})
+			elif(data['payload'] == '0'):
+				print("turns")
+				data = {'lights' : 0}
+				requests.post('http://localhost:5000/off', data=data)
+		# if(data['topic'] == "lightgoogle/auto"):
+		# 	return jsonify({'lights' : 2})
+
 
 # while True:
 # 	print("latching data to database")
@@ -155,22 +180,24 @@ def send_data_to_DB():
 send_data_to_DB();
 
 #main route
-@app.route('/')
+@app.route('/', methods = ['POST', 'GET'])
 def index():
+
 	return render_template('index.html')
 
 @app.route('/on', methods = ['POST', 'GET'])
 def turn_light_on():
 	if request.method == 'POST':
-		mqtt.publish('lights/on_off', 1)
 		mqtt.publish('lights/auto', 0)
+		mqtt.publish('lights/on_off', 1)	
 	return jsonify({'lights' : 1})
 
 @app.route('/off', methods = ['POST', 'GET'])
 def turn_light_off():
+	print("light off")
 	if request.method == 'POST':
-		mqtt.publish('lights/on_off', 0)
 		mqtt.publish('lights/auto', 0)
+		mqtt.publish('lights/on_off', 0)
 	return jsonify({'lights' : 0})
 
 @app.route('/auto', methods = ['POST', 'GET'])
@@ -178,7 +205,20 @@ def turn_light_auto():
 	if request.method == 'POST':
 		# mqtt.publish('lights/on_off', 0)
 		mqtt.publish('lights/auto', 1)
-	return jsonify({'lights' : 0})
+	return jsonify({'lights' : 2})
+
+@app.route('/temp', methods = ['POST', 'GET'])
+def get_temperature_stats():
+	if request.method == 'POST':
+		# temp = request.args.get('temp')
+		temp = []
+		queries = DB_data.objects()
+		# temps = db.d_b_data.find({}, {temperature:1, timestamp:1})
+
+		for i in queries:
+			temp.append(i.getTemp())
+		print(temp)
+		return jsonify({'temp' : temp})
 
 
 if __name__ == '__main__':
